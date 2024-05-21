d7814aae60592903e38a16d7d6b0927b9a27877f
Muhammad Syairozy Syauqi <syairozys@gmail.com>
2024-05-21 14:20:50 +0700

    test

--- resources/js/Pages/Laracasts/Components/Layout.jsx
+++ resources/js/Pages/Laracasts/Components/Layout.jsx
@@ -207,7 +207,7 @@ export default function Example({ children, heading }) {
                 <header className="bg-white shadow">
                     <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                         <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-                            {heading} Page
+                            {heading}
                         </h1>
                     </div>
                 </header>
--- resources/js/Pages/Laracasts/Jobs.jsx
+++ resources/js/Pages/Laracasts/Jobs.jsx
@@ -2,11 +2,32 @@ import { Head } from "@inertiajs/react";
 import Layout from "./Components/Layout";
 
 export default function Jobs() {
+    const jobs = [
+        {
+            title: "Director",
+            salary: 50,
+        },
+        {
+            title: "Programmer",
+            salary: 20,
+        },
+        {
+            title: "Teacher",
+            salary: 20,
+        },
+    ];
+
+    const jobList = jobs.map((job) => (
+        <li key={job.title}>
+            <b className="font-bold">{job.title}</b>: Pays {job.salary} per year
+        </li>
+    ));
+
     return (
         <>
             <Head title="Jobs" />
-            <Layout heading={"Jobs"}>
-                <p>Jobs</p>
+            <Layout heading={"Job Listings"}>
+                <ul className="list-disc list-inside ml-2">{jobList}</ul>
             </Layout>
         </>
     );
